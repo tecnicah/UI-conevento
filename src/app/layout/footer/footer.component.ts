193510f11493e35175122d4f4b74c7e33552b8b4
alfonsominimalist <40670147+alfonsominimalist@users.noreply.github.com>
2022-01-11 16:00:40 -0600

    cambios en footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
 
+
+  public openTInNewWindow() {
+    // Converts the route into a string that can be used 
+    // with the window.open() function
+    const url = this.router.serializeUrl(this.router.createUrlTree(['/Wizard'])
+    );
+  
+    window.open(url, '_blank');
+  }
+
+  public openAInNewWindow() {
+    // Converts the route into a string that can be used 
+    // with the window.open() function
+    const url = this.router.serializeUrl(
+      this.router.createUrlTree(['/Wizard'])
+    );
+  
+    window.open(url, '_blank');
+  }
 }
