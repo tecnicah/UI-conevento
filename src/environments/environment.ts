8c53b9323ff1e19e6bdb14407e3dab5307742f03
alfonsominimalist <40670147+alfonsominimalist@users.noreply.github.com>
2022-01-16 18:16:12 -0600

    version con las opciones del nav centradas
    version con las opciones del nav centradas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  //API_URL: 'https://my.premierds.com/API-conevento/api/',
+  API_URL: 'https://my.premierds.com/API-conevento/api/',
 
  //  API_URL: 'http://34.237.214.147/back/api_conevento/api/',
   images_path: "http://34.237.214.147/back/api_conevento/api/",
-   API_URL: 'https://localhost:44382/api/',
+  // API_URL: 'https://localhost:44382/api/',
    //images_path: "http://34.237.214.147/back/api_conevento/api/",
    clientId: "Abuwu5AU5gDm9Zbuqyby145mPlJbr7wJFQBxOiQJOXPW1_sKfnHgrD7UY7UVUdSvlqe6dseZFLvTL8BI",
    url_base: "http://34.237.214.147/front/conevento/",
