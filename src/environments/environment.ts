dd601f1f4efede3223732631afb7e0044cdf9f3f
alfonsominimalist <40670147+alfonsominimalist@users.noreply.github.com>
2022-01-29 11:16:17 -0600

    rnviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  //API_URL: 'https://my.premierds.com/API-conevento/api/',
+  API_URL: 'https://my.premierds.com/API-conevento/api/',
 
-  API_URL: 'http://34.237.214.147/back/api_conevento/api/',
+ // API_URL: 'http://34.237.214.147/back/api_conevento/api/',
   images_path: "http://34.237.214.147/back/api_conevento/api/",
    //API_URL: 'https://localhost:44382/api/',
    //images_path: "http://34.237.214.147/back/api_conevento/api/",
